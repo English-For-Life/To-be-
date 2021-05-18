7ee291411e5d1e10ce7b2197ffdbbaa521a4c3a1
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 12:31:57 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"She ___ my best friend.",
+        options:["-", "is", "are", "am"],
+        answer: 1
+    },
+    {
+     q:"I ___ a doctor.",
+        options:["am not", "are not", "is not", "do not"],
+        answer: 0   
+    },
+    {
+        q:"___ you married?",
+        options:["Am", "Is", "-", "Are"],
+        answer: 3
+    },
+    {
+        q:"They ___ in Paris.",
+        options:["is", "-", "am", "are"],
+        answer: 3
+    },
+    {
+        q:"Sara ___ at school today.",
+        options:["does not", "are not", "is not", "do not"],
+        answer: 2
+    },
+    {
+        q:"___ Brad Pitt from the USA?",
+        options:["Are", "Is", "-", "Does"],
+        answer: 1
+    },
+    {
+        q:"Kevin and Alex ___ on holiday.",
+        options:["are", "-", "is", "am"],
+        answer: 0
+    },
+    {
+        q:"We ___ good friends.",
+        options:["does not", "is not", "do not", "are not"],
+        answer: 3
+    },
+    {
+        q:"I ___ an English student.",
+        options:["-", "am", "is", "are"],
+        answer: 1
+    },
+    {
+        q:"What ___ your favourite movie?",
+        options:["does", "are", "is", "-"],
+        answer: 2
+    }
+
+]
